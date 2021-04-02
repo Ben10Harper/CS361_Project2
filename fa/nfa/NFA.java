d1c1f2baa08c86154613da6209f54893d7b8b428
Ben10Harper <bennyharper@gmail.com>
2021-04-02 12:28:02 -0600

    added more comments

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -9,6 +9,14 @@ import fa.State;
 import fa.dfa.DFA;
 import fa.dfa.DFAState;
 
+/**
+ * March 25, 2021
+ * The class stores all the information read
+ * from the input file, and creates the states
+ * in order to simulate the given NFA, then
+ * converts the given NFA into a DFA
+ * @author Ben Harper and Andrew Haddon
+ */
 public class NFA implements NFAInterface {
 	private Set<NFAState> Q;
 	private Set<NFAState> F;
@@ -23,7 +31,7 @@ public class NFA implements NFAInterface {
 
 	/**
 	 * Adds the start state as well as adding it to the main list
-	 * @param STRING name of the state typical its alphabetical symbol
+	 * @param STRING name - name of the state typically its alphabetical symbol
 
 	 */
 	@Override
@@ -42,7 +50,7 @@ public class NFA implements NFAInterface {
 
 	/**
 	 * Adds a state to the main list
-	 * @param STRING name of the state typical its alphabetical symbol
+	 * @param STRING name - name of the state typically its alphabetical symbol
 	 */
 	@Override
 	public void addState(String name) {
@@ -55,9 +63,10 @@ public class NFA implements NFAInterface {
 			Q.add(state);
 		}
 	}
+	
 	/**
-	 * Adds a state to list of end states and 
-	 * @param STRING name of the state typical its alphabetical symbol
+	 * Adds a state to list of final states and all states 
+	 * @param STRING name - name of the state typically its alphabetical symbol
 	 */
 	@Override
 	public void addFinalState(String name) {
@@ -71,11 +80,13 @@ public class NFA implements NFAInterface {
 			F.add(state);
 		}
 	}
+	
 	/**
-	 * Adds transition, symbol my symbol recursively 
-	 * @param STRING fromState name of the state the transition is from
-	 * @param CHAR onSymb the symbol that the transition happens on
-	 * @param STRING toState name of the state transition ends on
+	 * Adds transition to the state's delta map, with the symbol as the
+	 * key and the toState as the value 
+	 * @param STRING fromState - name of the state the transition is from
+	 * @param CHAR onSymb - the symbol that the transition happens on
+	 * @param STRING toState - name of the state transition ends on
 	 */
 	@Override
 	public void addTransition(String fromState, char onSymb, String toState) {
@@ -86,24 +97,26 @@ public class NFA implements NFAInterface {
 			System.out.println("One of those states doesn't exist!");
 			return;
 		}
-		//recursion 
+		//adds to the fromState's delta map
 		from.addTransition(onSymb, to);
-		//makes a list of Symbols already used on a state
+		//adds to the list of symbols used in the machine
 		if(!sigma.contains(onSymb)){
 			sigma.add(onSymb);
 		}
 	}
+	
 	/**
-	 * Getter for Q
-	 * @return SET a set of states that FA has
+	 * Getter for Q (All States)
+	 * @return Set<NFAState> - a set of all states that FA has
 	 */
 	@Override
 	public Set<? extends State> getStates() {
 		return Q;
 	}
+	
 	/**
-	 * Getter for F
-	 * @return SET a set of final states that FA has
+	 * Getter for F (Final States)
+	 * @return Set<NFAState> - a set of final states that FA has
 	 */
 	@Override
 	public Set<? extends State> getFinalStates() {
@@ -111,16 +124,17 @@ public class NFA implements NFAInterface {
 	}
 
 	/**
-	 * Getter for Q
-	 * @return State retuns the start state
+	 * Getter for q0 (the start state)
+	 * @return State - returns the start state
 	 */
 	@Override
 	public State getStartState() {
 		return q0;
 	}
+	
 	/**
-	 * Getter for Q
-	 * @return Set a set of transition symbols 
+	 * Getter for sigma (set of alphabet symbols)
+	 * @return Set<Character> - a set of transition symbols 
 	 */
 	@Override
 	public Set<Character> getABC() {
@@ -128,27 +142,29 @@ public class NFA implements NFAInterface {
 	}
 
 	/**
-	 * Converts a NFA to DFA
-	 * @param none/the whole program
-	 * @return DFA
+	 * Converts our NFA into a DFA
+	 * @return DFA - the new DFA converted from our NFA
 	 */
 	@Override
 	public DFA getDFA() {
-		//starts a new DFA
+		//creates a new DFA
 		DFA dfa = new DFA();
+		//creates the queue for BFS and set of visited state so we don't have repeated states
 		Queue<Set<NFAState>> queue = new LinkedList<Set<NFAState>>();
 		Set<Set<NFAState>> visited = new LinkedHashSet<Set<NFAState>>();
+		//gets the new start state for the DFA
 		Set<NFAState> startState = eClosure((NFAState) getStartState());
-		//checks if start and final state are the same
+		//checks if start state is a final state
 		if (isFinal(startState)) {
 			dfa.addFinalState(startState.toString());
 		}
-		//sets start state and adds to to que and visted
+		//sets DFA's start state and adds it to the queue and the visited set
 		dfa.addStartState(startState.toString());
 		queue.add(startState);
 		visited.add(startState);
 		//goes though the queue set to add all elements from NFA to DFA
 		while (!queue.isEmpty()) {
+			//takes oldest state from the queue
 			Set<NFAState> currStates = queue.remove();
 			//goes though each transition symbol 
 			for (Character c : sigma) {
@@ -158,73 +174,94 @@ public class NFA implements NFAInterface {
 				}
 				//starts the transition state that is used for the rest of this method
 				Set<NFAState> tranState = new LinkedHashSet<NFAState>();
-				//
+				//goes through each individual state in the set from the queue
 				for (NFAState state : currStates) {
+					//checks if the state has a transition on this char
 					if (state.getTo(c) != null) {
+						//adds all the possible states it could go to
 						tranState.addAll(state.getTo(c));
+						//loops through tranState and runs eclosure on each state to add free transitions
 						for (NFAState s : tranState) {
+							//adds the resulting states back into tranState
 							tranState.addAll(eClosure(s));
 						}
 					}
 				}
+				//checks if the same state has been visited, but the states are in different orders
 				Set<NFAState> equivilantState = equalState(tranState, visited);
 				if (equivilantState != null) {
+					//change the new state to the order of the old equivalent one
 					tranState = equivilantState;
 				}
+				//checks if state has already been visited
 				if (!visited.contains(tranState)) {
+					//if not, add to the queue then add to visisted
 					queue.add(tranState);
 					visited.add(tranState);
 				}
+				//checks if the state is already in the DFA
 				if (!isDFAState(tranState.toString(), dfa)) {
+					//if not, check if it's final or not and add appropriately
 					if (isFinal(tranState)) {
 						dfa.addFinalState(tranState.toString());
 					} else {
 						dfa.addState(tranState.toString());
 					}
 				}
+				//add the dfa transition
 				dfa.addTransition(currStates.toString(), c, tranState.toString());
 			}
 		}
+		//returns the newly made dfa
 		return dfa;
 	}
+	
 	/**
-	 * Getter for the state transtion to from a state on a symbol
-	 * @param NFASTATE from The state we want the transition on
-	 * @param CHAR onSymb the symbol that the transition happens on
-	 * @return SET returns a NFA state that the symbol leads to
+	 * Getter for the state the given state transitions to on the given symbol
+	 * @param NFAState from - The state we want the transition from
+	 * @param CHAR onSymb - the symbol that the transition happens on
+	 * @return Set<NFAState> - returns a NFA state that the symbol leads to
 	 */
 	@Override
 	public Set<NFAState> getToState(NFAState from, char onSymb) {
 		return from.getTo(onSymb);
 	}
+	
 	/**
 	 * Gets all the states you can get to on a e transition 
-	 * @param NFASTATE from The state we want to trace the e Transitions 
-	 * @return SET NFASTATES returns a Set of nfa states that you can visit off e transition
+	 * @param NFAState from - The state we want to trace the e Transitions 
+	 * @return Set<NFAState> - returns a Set of nfa states that you can visit off e transition
 	 */
 	@Override
 	public Set<NFAState> eClosure(NFAState s) {
-		//creates sets to keep track of where we have been
+		//creates sets to keep track of where we have been and where we can get to
 		Set<NFAState> eStates = new LinkedHashSet<NFAState>();
 		Set<NFAState> visited = new LinkedHashSet<NFAState>();
+		//adds the given state to both
 		eStates.add(s);
 		visited.add(s);
-		//gets any transitions on e
+		//gets all states s can get to using e transitions
 		Set<NFAState> setOfStates = s.getTo('e');
-		//adds to the list of visted to avoid looping
+		//checks if there are any states returned
 		if (setOfStates != null) {
+			//loops through each state gotten from the e transitions
 			for (NFAState state : setOfStates) {
+				//recursively calls eclosure on each new state and adds to eStates
+				//in the end this leaves us with all possible states that can be visited
+				//for free from the given state s
 				eStates.addAll(eClosure(state, visited));
 			}
 		}
 		//System.out.println(eStates);
 		return eStates;
 	}
+	
 	/**
-	 * Getter for the state transition to from a state on a symbol
-	 * @param NFASTATES from The state we are e tracing
-	 * @param SET NFASTATES Set of visited states
-	 * @return SET NFASTATES returns a set of eStates
+	 * Gets all the states you can get to on a e transition 
+	 * (same as above but includes the visited list)
+	 * @param NFAState s - The state we are e tracing
+	 * @param Set<NFAState> visited - Set of visited states
+	 * @return Set<NFAState> - returns a set of eStates
 	 */
 	public Set<NFAState> eClosure(NFAState s, Set<NFAState> visited) {
 		//creates a new list of estates
@@ -233,20 +270,24 @@ public class NFA implements NFAInterface {
 		visited.add(s);
 		//gets any transitions on e
 		Set<NFAState> setOfStates = s.getTo('e');
+		//makes sure those states exist
 		if (setOfStates != null) {
-			//if the state is not visted add the state to both the visted and e state list
+			//loops through all the states
 			for (NFAState state : setOfStates) {
+				//checks if we've already done the eClosure on that state
 				if (!visited.contains(state)) {
+					//recursively calls eClosure on each new state
 					eStates.addAll(eClosure(state, visited));
 				}
 			}
 		}
 		return eStates;
 	}
+	
 	/**
-	 * Utility that checks if a state already exits
-	 * @param String the name of the state
-	 * @return NFASTATE returns the state if it already exists, or null if it does not.
+	 * Utility that checks if a state already exits in Q
+	 * @param String name - the name of the state
+	 * @return NFAState - returns the state if it already exists, or null if it does not.
 	 */
 	private NFAState alreadyExists(String name) {
 		NFAState existing = null;
@@ -259,10 +300,11 @@ public class NFA implements NFAInterface {
 		}
 		return existing;
 	}
+	
 	/**
 	 * Utility that checks if the state is a final state
-	 * @param SET NFASTATES A set of states
-	 * @return Boolean if it contains a final state returns true else false
+	 * @param Set<NFAState> states - A set of states to test
+	 * @return Boolean - if it contains a final state returns true else false
 	 */
 	private boolean isFinal(Set<NFAState> states) {
 		boolean fin = false;
@@ -274,17 +316,18 @@ public class NFA implements NFAInterface {
 		}
 		return fin;
 	}
+	
 	/**
-	 * Utility that checks if a name is a state
-	 * @param STRING nstate the name of a state
-	 * @parm dfa the dfa to check if for states
-	 * @return DFA Boolean true false
+	 * Utility that checks a state is already in the DFA
+	 * @param STRING - nstate the name of a state
+	 * @param DFA dfa - the dfa to check for states
+	 * @return Boolean - returns true or false if the state is in the DFA or not
 	 */
 	private boolean isDFAState(String nstate, DFA dfa) {
 		boolean exists = false;
 		//creates the set to check against
 		Set<DFAState> dfaStates = dfa.getStates();
-		//loops though to check all states agaist name
+		//loops though to check all states against name
 		for (DFAState dstate : dfaStates) {
 			if(nstate.equals(dstate.getName())) {
 				exists = true;
@@ -293,16 +336,20 @@ public class NFA implements NFAInterface {
 		}
 		return exists;
 	}
+	
 	/**
-	 * Utility that checks if two sates are the same
-	 * @param SET NFASTATES testState the first state
-	 * @parm SET NFASTATES of visted NFA states
-	 * @return SET NFASTATES the equivalent NFAstate
+	 * Utility that checks if two sates are the same but in different orders
+	 * @param Set<NFAState> testState - the first state
+	 * @parm Set<Set<NFAState>> visited - set of sets to test against
+	 * @return Set<NFAState> the equivalent NFAstate, or null if one doesn't exist
 	 */
 	private Set<NFAState> equalState(Set<NFAState> testState, Set<Set<NFAState>> visited) {
 		Set<NFAState> equivilantState = null;
+		//loops through all sets of states in visited
 		for (Set<NFAState> state : visited) {
+			//checks if they're equal
 			if(testState.equals(state)) {
+				//if so, return the visited state
 				equivilantState = state;
 				break;
 			}
--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -4,45 +4,65 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+/**
+ * March 25, 2021
+ * The class stores all the information
+ * for each state, such as its name
+ * and the map to find it's next state.
+ * It also contains methods to set and
+ * get the state's information.
+ * @author Ben Harper and Andrew Haddon
+ */
 public class NFAState extends fa.State {
 
+	//HashMap to store transitions
 	private HashMap<Character, Set<NFAState>> delta;
 	
 	/**
 	 * Starts a new nfa state
-	 * @param String name the name of the state
+	 * @param String name - the name of the state
 	 */
 	public NFAState(String name) {
 		newState(name);
 	}
+	
 	/**
-	 * Starts a new nfa state and adds it to the set
+	 * Creates the new state, giving it a name and new delta map
 	 * @param String name the name of the state
 	 */
 	private void newState(String name){
 		this.name = name;
 		delta = new HashMap<Character, Set<NFAState>>();
 	}
+	
 	/**
 	 * Adds a transition to existing states 
-	 * @param NFAsstate toState the state is transitions to on symbol
-	 * @param Char OnSymb the symbol to transition to the new state on.
+	 * @param Char onSymb - the symbol it transitions on
+	 * @param NFAState toState - the state that it goes to on the given symbol
 	 */
 	public void addTransition(char onSymb, NFAState toState){
+		//creates a new set of NFAState's
 		Set<NFAState> set = new LinkedHashSet<NFAState>();
+		//checks if it already has a set of states on that transition
 		if (delta.get(onSymb) != null) {
+			//sets the new set to the contents of the old set
 			set = delta.get(onSymb);
 		}
+		//adds the new state to the set
 		set.add(toState);
+		//puts the set back
 		delta.put(onSymb, set);
 	}
+	
 	/**
-	 * Finds all the states the symbol transitions too. 
-	 * @param Char symb
-	 * @return Set NFAState a set of all states the symbol transtions to 
+	 * Finds all the states this state transitions to on the given symbol 
+	 * @param Char symb - the symbol to be used as the key in the map
+	 * @return Set<NFAState> - a set of all states this state transitions to on the symbol 
 	 */
 	public Set<NFAState> getTo(char symb){
+		//gets the set of states
 		Set<NFAState> toStates = delta.get(symb);
+		//checks if it exists
 		if(toStates != null) {
 			return toStates;
 		} else if (toStates == null && symb == 'e') {
